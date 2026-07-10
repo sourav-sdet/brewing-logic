d56b1ab3e8b4567a89336e44a2606cabc33db2ae
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-10 19:26:35 +0530

    Index of Last Occurrence in Sorted Array

--- searching/IndexOfFirstOccurrenceSorted.java
+++ searching/IndexOfFirstOccurrenceSorted.java
@@ -28,12 +28,44 @@ public class IndexOfFirstOccurrenceSorted {
 
     }
 
+
+
+    public static int indexOfLastOccurrence(int arr[],int n, int x){
+
+        int low=0, high=n-1;
+
+        while(low<=high){
+
+            int mid = (low + high)/2;
+            if(x > arr[mid])
+                low = mid+1;
+            else if(x < arr[mid])
+                high = mid-1;
+
+            else{
+                // Check if its the last occurrence
+                if(mid == n-1 || (arr[mid] != arr[mid+1]))
+                    return mid;
+                else 
+                    low = mid+1;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+
     public static void main(String args[]){
 
-        int arr[] ={10,20,20,30,30,40,50,60};
+        int arr[] ={10,20,20,30,30,30,40,50,60};
         
-        int result = indexOfFirstOccurrence(arr, arr.length,30);
-        System.out.println(result);
+        int indexFirst = indexOfFirstOccurrence(arr, arr.length,10);
+        System.out.println(indexFirst);
+
+        int indexLast = indexOfLastOccurrence(arr, arr.length,10);
+        System.out.println(indexLast);
         
 		
     }
