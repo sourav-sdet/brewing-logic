5bef2075797e92eba01d35fa3a2100eeb8aa6b0d
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-10 09:33:56 +0530

    Binary Search Recursive

--- searching/BinarySearchIterative.java
+++ searching/BinarySearchIterative.java
@@ -21,10 +21,28 @@ public class BinarySearchIterative {
     }
 
 
+    public static int binarySearchRecursive(int arr[], int low, int high, int x){
+        // BASE CASE
+        if (low > high)
+            return -1;
+
+        int mid = (low + high)/2;
+
+        if(arr[mid] == x)
+            return mid;
+        else if(x < arr[mid])
+            return binarySearchRecursive(arr, low, mid-1, x);
+        else
+            return binarySearchRecursive(arr, mid+1, high, x);
+
+    }
+
+
     public static void main(String args[]){
 
         int arr[] ={10,20,30,40,50,60};
-        int result = binarySearchIterative(arr, arr.length,70);
+        //int result = binarySearchIterative(arr, arr.length,70);
+        int result = binarySearchRecursive(arr, 0,arr.length-1,20);
         System.out.println(result);
         
 		
