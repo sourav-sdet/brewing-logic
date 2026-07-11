836043361ea97a1c0d7432dbd1aa1f4c1125455e
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-11 08:34:08 +0530

    Count Occurrences in Sorted Array

--- searching/IndexOfFirstOccurrenceSorted.java
+++ searching/IndexOfFirstOccurrenceSorted.java
@@ -57,15 +57,33 @@ public class IndexOfFirstOccurrenceSorted {
     }
 
 
+    public static int countNumberOfOccurrences(int arr[], int n, int x){
+
+        // Find the index of First Occurrence of x
+        int first=indexOfFirstOccurrence(arr, n, x);
+
+        if(first == -1)
+            return 0;
+
+        else 
+            return (indexOfLastOccurrence(arr, n, x) - first) +1;
+
+
+    }
+
+
     public static void main(String args[]){
 
         int arr[] ={10,20,20,30,30,30,40,50,60};
         
-        int indexFirst = indexOfFirstOccurrence(arr, arr.length,10);
+        int indexFirst = indexOfFirstOccurrence(arr, arr.length,30);
         System.out.println(indexFirst);
 
-        int indexLast = indexOfLastOccurrence(arr, arr.length,10);
+        int indexLast = indexOfLastOccurrence(arr, arr.length,30);
         System.out.println(indexLast);
+
+        int countOccurrences = countNumberOfOccurrences(arr, arr.length, 30);
+        System.out.println(countOccurrences);
         
 		
     }
