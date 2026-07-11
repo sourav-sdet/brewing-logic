2c38812750ab5c67c163237e5bccb951c5952f58
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-11 09:07:05 +0530

    Square Root using Binary Search

--- searching/SquareRoot.java
+++ searching/SquareRoot.java
@@ -13,14 +13,40 @@ public class SquareRoot {
 
     }
 
+    public static int squareRootFloor(int num){
+        
+        // Use Binary Search
+        // We have a range from 1 to num
+        int low=1, high=num;
+        int result=-1;
 
+        while(low <= high){
 
+            int mid = (low + high)/2;
+            int midSq= mid * mid;
 
+            // Check if mid is a square root
+            if(midSq == num)
+                return mid;
 
+            // If mid square is greater than num, go to left half
+            else if(midSq > num)
+                high = mid -1;
+
+            // if mid square is smaller than num, store the result and go to the right half
+            else{
+                low = mid +1;
+                result = mid;
+            }
+
+        }
+
+        return result;
+    }
 
     public static void main(String args[]){
 
-        int result = squareRootFloorNaive(15);
+        int result = squareRootFloor(25);
         System.out.println(result);
 
     }
