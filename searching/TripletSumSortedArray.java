ed27779259e2e838ea8405fa8122c437c4fdaa42
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-13 20:08:41 +0530

    Triplet in Sorted Array using Two Pointers

--- searching/TripletSumSortedArray.java
+++ searching/TripletSumSortedArray.java
@@ -17,10 +17,40 @@ public class TripletSumSortedArray {
 
     }
 
+
+    public static boolean isPairSum(int arr[], int n, int sum, int s){
+
+        // TWO POINTER TECHNIQUE
+        int left = s, right = n-1;
+
+        while(left < right){
+            if(arr[left] + arr[right] == sum)
+                return true;
+            else if(arr[left] + arr[right] > sum)
+                right --;
+            else
+                left ++;
+        }
+        return false;
+
+
+    }
+
+
+    public static boolean isTripletSum(int arr[], int n, int sum){
+
+        for(int i=0; i< n-2; i++){
+            if(isPairSum(arr, n, sum-arr[i], i+1))
+                return true;
+        }
+        return false;
+
+    }
+
     public static void main(String args[]){
 
         int arr[]={2,4,8,9,20,40};
-        boolean result = isTripletSumNaive(arr, arr.length,32 );
+        boolean result = isTripletSum(arr, arr.length,32 );
         System.out.println(result);
 
     }
