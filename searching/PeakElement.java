2a18992b00cbc40d1d0ab739025e43ec3bcdf297
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-13 09:06:37 +0530

    Peak Element Binary Search Implementation

--- searching/PeakElement.java
+++ searching/PeakElement.java
@@ -17,10 +17,33 @@ public class PeakElement {
 
     }
 
+    public static int peakElement(int arr[], int n){
+
+        int low=0, high=n-1;
+        while(low <= high){
+
+            int mid = (low + high)/2;
+
+            // Check if mid is a peak element
+            if ((mid == 0 || arr[mid-1] <= arr[mid]) && (mid == n-1 || arr[mid+1] <= arr[mid]))
+                return arr[mid];
+
+            // If mid is not peak element, check if left of mid is greater or equal to mid
+            if( mid > 0 && arr[mid -1] >= arr[mid])
+                high = mid -1;
+            else 
+                low = mid +1;
+
+        }
+        return -1;
+
+    }
+
+
     public static void main(String args[]){
 
         int arr[]={6,7,8,20,12};
-        int result = peakElementNaive(arr, arr.length);
+        int result = peakElement(arr, arr.length);
         System.out.println(result);
 
     }
