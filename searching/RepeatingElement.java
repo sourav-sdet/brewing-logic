4ec7337697bf23282dc37ea282ca34ebad028398
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-14 18:47:08 +0530

    Repeating Element Best Approach

--- searching/RepeatingElement.java
+++ searching/RepeatingElement.java
@@ -29,13 +29,50 @@ public class RepeatingElement {
 
     }
 
+    public static int repeatingElementEfficient(int arr[], int n){
 
+        boolean visited[] = new boolean[n];
+
+        for(int i=0; i<n; i++){
+
+            if(visited[arr[i]])
+                return arr[i];
+            visited[arr[i]] = true;
+
+        }
+        return -1;
+
+    }
+    
+
+    public static int repeatingElement(int arr[], int n){
+
+        // USING FAST AND SLOW POINTERS
+        int slow = arr[0]+1, fast = arr[0]+1;
+
+        // PHASE 1: Move slow one link at a time, move fast 2 links at a time
+        do{
+            slow = arr[slow]+1;
+            fast = arr[arr[fast]+1]+1;
+        }while(slow != fast);
+
+        // PHASE 2: Move slow to the beginning, then move both slow and fast at the same pace
+        // They will meet at the starting point of the cycle which is the repeating element
+        slow = arr[0]+1;
+        while(slow != fast){
+            slow = arr[slow]+1;
+            fast = arr[fast]+1;
+        }
+        return slow-1;
+
+
+    }
 
 
     public static void main(String args[]){
 
         int arr[]={0,2,1,3,2,2};
-        int result= repeatingElementNaive(arr, arr.length);
+        int result= repeatingElement(arr, arr.length);
         System.out.println(result);
 
     }
