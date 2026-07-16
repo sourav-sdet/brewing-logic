c7d433caf2b9bcf280519d2770ddf508ad944fb1
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-16 17:45:27 +0530

    String Palindrome Check Efficient Solution

--- strings/PalindromeCheck.java
+++ strings/PalindromeCheck.java
@@ -10,14 +10,27 @@ public class PalindromeCheck {
 
     }
 
+    public static boolean isPalindrome(String str){
 
+        int begin = 0, end = str.length() -1;
+
+        while(begin < end){
+            if(str.charAt(begin) != str.charAt(end))
+                return false;
+            begin ++;
+            end --;
+        }
+        return true;
+
+
+    }
 
 
 
     public static void main(String args[]){
 
-        String str = "geeksforgeeks";
-        System.out.println(isPalindromeNaive(str));
+        String str = "ABBAC";
+        System.out.println(isPalindrome(str));
         	
     }
     
