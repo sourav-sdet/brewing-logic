08bbad2af89e134e31bea448bef728df673340ba
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-17 21:19:59 +0530

    String Anagram Check Effective Solution

--- strings/AnagramCheck.java
+++ strings/AnagramCheck.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class AnagramCheck {
 
+    static final int CHAR = 256;
+
     public static boolean areAnagramsNaive(String s1, String s2){
 
         if(s1.length() != s2.length())
@@ -20,17 +22,35 @@ public class AnagramCheck {
     }
 
 
+    public static boolean areAnagrams(String s1, String s2){
+
+        if(s1.length() != s2.length())
+            return false;
+
+        int count[] = new int[CHAR];
+
+        for(int i=0; i<s1.length(); i++){
+
+            count[s1.charAt(i)]++;
+            count[s2.charAt(i)]--;
+
+        }
+
+        for(int i=0; i<CHAR; i++){
+            if(count[i] != 0)
+                return false;
+        }
+
+        return true;
 
 
-
-
-
+    }
 
     public static void main(String args[]){
 
         String s1 = "silent";
-        String s2 = "listens";
-        System.out.println(areAnagramsNaive(s1,s2));
+        String s2 = "listen";
+        System.out.println(areAnagrams(s1,s2));
         	
     }
     
