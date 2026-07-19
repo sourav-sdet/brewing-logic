88f1bfdf24bf06f83b2b96b96c7fcc99d0b2b866
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-19 08:54:29 +0530

    Leftmost Repeating Character Efficient 1

--- strings/LeftmostRepeatingCharacter.java
+++ strings/LeftmostRepeatingCharacter.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.Arrays;
+
 public class LeftmostRepeatingCharacter {
 
     public static int leftMostRepeatingCharacterNaive(String str){
@@ -36,10 +38,34 @@ public class LeftmostRepeatingCharacter {
 
     }
 
+    public static int leftMostRepeatingCharacterEfficient(String str){
+
+        // Traverse the string from left to right
+        // Keep track of index of first occurrence of every character
+        int fIndex[] = new int[256];
+        Arrays.fill(fIndex, -1);
+
+        int res = Integer.MAX_VALUE;
+
+        for(int i=0; i<str.length(); i++){
+
+            int fi = fIndex[str.charAt(i)];
+
+            if (fi == -1)
+                fIndex[str.charAt(i)] = i;
+            else 
+                res = Math.min(res, fi);
+
+        }
+
+        return (res==Integer.MAX_VALUE)?-1:res;
+
+    }
+
     public static void main(String args[]){
 
         String str = "abccbd";
-        System.out.println(leftMostRepeatingCharacterBetter(str));
+        System.out.println(leftMostRepeatingCharacterEfficient(str));
         	
     }
     
