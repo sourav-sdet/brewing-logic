d96fbbd8f05f9bd0eb17e482d5dab42712442688
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-19 09:29:19 +0530

    Left Most Non Repeating Character Naive Solution

---
+++ strings/LeftmostNonRepeatingCharacter.java
@@ -0,0 +1,32 @@
+package strings;
+
+public class LeftmostNonRepeatingCharacter {
+
+    public static int leftMostNonRepeatingCharacterNaive(String str){
+
+        
+        for(int i=0; i<str.length(); i++){
+            boolean flag = false;
+            for(int j=0; j<str.length(); j++){
+                if(i != j && str.charAt(i) == str.charAt(j)){
+                    flag = true;
+                    break;
+                }
+                    
+            }
+            if (flag == false)
+                return i;
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String args[]){
+
+        String str = "geeksforgeeks";
+        System.out.println(leftMostNonRepeatingCharacterNaive(str));
+        	
+    }
+    
+}
