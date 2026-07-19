74bcc6153c0e072454c4876c0b2c19fe932a4003
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-19 09:37:26 +0530

    Left Most Non Repeating Character Efficient Solution

--- strings/LeftmostNonRepeatingCharacter.java
+++ strings/LeftmostNonRepeatingCharacter.java
@@ -21,11 +21,27 @@ public class LeftmostNonRepeatingCharacter {
         return -1;
     }
 
+    public static int leftMostNonRepeatingCharacterBetter(String str){
+
+        int count[] = new int[256];
+
+        for(int i=0; i< str.length(); i++){
+            count[str.charAt(i)]++;
+        }
+
+        for(int i=0; i< str.length(); i++){
+            if(count[str.charAt(i)] == 1)
+                return i;
+        }
+
+        return -1;
+
+    }
 
     public static void main(String args[]){
 
         String str = "geeksforgeeks";
-        System.out.println(leftMostNonRepeatingCharacterNaive(str));
+        System.out.println(leftMostNonRepeatingCharacterBetter(str));
         	
     }
     
