c102a776caacb05ceaff91e257a44f708d82a797
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-21 20:16:17 +0530

    Pattern Searching With Distinct Pattern

--- strings/NaivePatternSearching.java
+++ strings/NaivePatternSearching.java
@@ -19,11 +19,35 @@ public class NaivePatternSearching {
 
     }
 
+
+    public static void patternSearchNaiveWithDistinctPattern(String txt, String pat){
+
+        int m=pat.length(), n=txt.length();
+
+        for(int i=0; i<=n-m;){
+            int j;
+            for(j=0; j<m; j++){
+                if(pat.charAt(j) != txt.charAt(i+j))
+                    break;
+            }
+
+            if(j == m) 
+                System.out.println(i+" ");
+
+            if(j==0)
+                i++;
+            else 
+                i=i+j;
+        }
+
+
+    }
+
     public static void main(String args[]){
 
         String txt="ABCABCD";
         String pat="ABCD";
-        patternSearchNaive(txt,pat);
+        patternSearchNaiveWithDistinctPattern(txt,pat);
 
 
     }
