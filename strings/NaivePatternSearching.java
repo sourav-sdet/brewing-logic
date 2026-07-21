6dea5356d66b291cc0e303ebf596dc371cadbe87
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-21 20:02:50 +0530

    Naive Pattern Searching

---
+++ strings/NaivePatternSearching.java
@@ -0,0 +1,31 @@
+package strings;
+
+public class NaivePatternSearching {
+
+    public static void patternSearchNaive(String txt, String pat){
+
+        int m=pat.length(), n=txt.length();
+
+        for(int i=0; i<=n-m; i++){
+            int j;
+            for(j=0; j<m; j++){
+                if(pat.charAt(j) != txt.charAt(i+j))
+                    break;
+            }
+            if(j == m)
+                    System.out.println(i+ " ");
+
+        }
+
+    }
+
+    public static void main(String args[]){
+
+        String txt="ABCABCD";
+        String pat="ABCD";
+        patternSearchNaive(txt,pat);
+
+
+    }
+    
+}
