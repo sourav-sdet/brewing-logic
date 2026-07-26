445a3a9e5094568775d0d02b733bd31fc8cf0c8e
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-26 20:14:23 +0530

    Pair With Given Sum Efficient Approach Using Hashing

--- hashing/PairWithGivenSum.java
+++ hashing/PairWithGivenSum.java
@@ -1,8 +1,10 @@
 package hashing;
 
+import java.util.HashSet;
+
 public class PairWithGivenSum {
 
-    public static boolean isPairNaive(int arr[], int sum){
+    public static boolean isPairSumNaive(int arr[], int sum){
 
         // Consider all possible pairs one by one and check its sum
         for(int i=0; i<arr.length; i++){
@@ -16,14 +18,31 @@ public class PairWithGivenSum {
 
 
 
+    public static boolean isPairSum(int arr[], int sum){
 
+        HashSet<Integer> h = new HashSet<>();
+
+        // Traverse through the input array
+        for(int x: arr){
+
+            // First check if sum - x is present in the HashSet
+            if(h.contains(sum - x))
+                return true;
+            else 
+                h.add(x);
+
+        }
+
+        return false;
+
+    }
 
 
 
     public static void main(String args[]){
 
-        int[] arr={8,3,9,4};
-        System.out.println(isPairNaive(arr, 20));
+        int[] arr={8,3,4,2,5};
+        System.out.println(isPairSumNaive(arr, 6));
         
 
 
