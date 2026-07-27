91769464629b201c7e0381c2db6a1edc653b619d
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-27 19:07:43 +0530

    SubArray with Zero Sum Efficient Solution using Prefix Sum and Hashing

--- hashing/SubArrayWithZeroSum.java
+++ hashing/SubArrayWithZeroSum.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashSet;
+
 public class SubArrayWithZeroSum {
 
     public static boolean subArrayWithZeroSumNaive(int arr[], int n){
@@ -17,13 +19,36 @@ public class SubArrayWithZeroSum {
     }
 
 
+    public static boolean subArrayWithZeroSum(int arr[], int n){
 
+        int pre_sum =0;
+        HashSet<Integer> h = new HashSet<>();
+
+        // Traverse through the Array from L --> R
+        for(int i=0; i<n; i++){
+
+            // Compute Prefix Sum for every element arr[i]
+            pre_sum = pre_sum + arr[i];
+
+            if(h.contains(pre_sum))
+                return true;
+
+            // Handle Case when whole Prefix has sum = 0
+            if(pre_sum == 0)
+                return true;
+
+            h.add(pre_sum);
+        }
+
+        return false;
+
+    }
 
 
     public static void main(String args[]){
 
         int[] arr={1,4,13,-3,-10,5};
-        System.out.println(subArrayWithZeroSumNaive(arr, arr.length));
+        System.out.println(subArrayWithZeroSum(arr, arr.length));
         
     }
     
