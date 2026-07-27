b92b6221baebd624351fe2b4c12445b4b7aebc07
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-27 19:29:13 +0530

    SubArray with given Sum using Prefix Sum and Hashing

--- hashing/PrintFrequencies.java
+++ hashing/PrintFrequencies.java
@@ -1,6 +1,6 @@
 package hashing;
 
-import java.util.HashMap;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
--- hashing/SubArrayWithGivenSum.java
+++ hashing/SubArrayWithGivenSum.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashSet;
+
 public class SubArrayWithGivenSum {
 
     
@@ -18,13 +20,31 @@ public class SubArrayWithGivenSum {
     
     
     
-    
-    
-    
-    
-    
-    
-    
+    public static boolean subArrayWithGivenSum(int arr[], int n, int sum){
+
+        int preSum=0;
+        HashSet<Integer> h = new HashSet<>();
+
+        // Traverse through the array from L --> R
+        for(int i=0; i<n; i++){
+
+            // Compute the preSum
+            preSum += arr[i];
+
+            // Check if preSum == given sum
+            if(preSum == sum)
+                return true;
+
+            if(h.contains(preSum - sum))
+                return true;
+
+            h.add(preSum);
+        }
+
+        return false;
+
+    }
+     
     public static void main(String args[]){
 
         int[] arr={5,8,6,13,3,-1};
