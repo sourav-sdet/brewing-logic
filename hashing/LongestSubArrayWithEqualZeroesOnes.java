be6ec827558e8c19f7dca43dc5c524525aca0ccc
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-28 19:08:42 +0530

    Longest SubArray with Equal Zeroes and Ones Efficient Solution

--- hashing/LongestSubArrayWithEqualZeroesOnes.java
+++ hashing/LongestSubArrayWithEqualZeroesOnes.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class LongestSubArrayWithEqualZeroesOnes {
 
     public static int longestSubArrayWithEqualZeroesOnesNaive(int arr[], int n){
@@ -31,12 +33,48 @@ public class LongestSubArrayWithEqualZeroesOnes {
     }
 
 
+    public static int longestSubArrayWithEqualZeroesOnes(int arr[], int n){
 
+        // Replace all 0s in the array with -1
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0)
+                arr[i] = -1;
+        }
+
+        // Use Length of Longest Sub Array with given Sum = 0
+        int res = 0, sum = 0, preSum=0;
+
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            // Compute the preSum
+            preSum = preSum + arr[i];
+
+            // Check if preSum == given sum
+            if(preSum == sum)
+                res = i+1;
+
+            // Check if hash map does not contain preSum
+            if(h.containsKey(preSum) == false){
+                h.put(preSum, i);
+            }
+
+            // Check if preSum-sum is present in hash map as a key
+            if(h.containsKey(preSum - sum)){
+                res = Math.max(res, i-h.get(preSum - sum));
+            }
+        }
+
+        return res;
+
+
+
+    }
 
     public static void main(String args[]){
 
         int arr[] ={1,1,1,0,1,0};
-		int result = longestSubArrayWithEqualZeroesOnesNaive(arr, arr.length);
+		int result = longestSubArrayWithEqualZeroesOnes(arr, arr.length);
         System.out.println(result);
 		
     }
