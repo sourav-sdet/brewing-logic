882c15897c4056f778a619345eb8c0f2133e7fc2
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-28 18:37:13 +0530

    LongestSubArray with Given Sum using Prefix Sum and Hashing

--- hashing/LongestSubArrayWithGivenSum.java
+++ hashing/LongestSubArrayWithGivenSum.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class LongestSubArrayWithGivenSum {
 
     public static int longestSubArrayWithGivenSumNaive(int arr[], int n, int sum){
@@ -28,14 +30,38 @@ public class LongestSubArrayWithGivenSum {
 
 
 
+    public static int longestSubArrayWithGivenSum(int arr[], int n, int sum){
 
+        int res=0;
+        int preSum =0;
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        // Traverse through the array from left to right
+        for(int i=0; i<n; i++){
+
+            // Compute the prefix sum
+            preSum = preSum + arr[i];
+
+            // If preSum is not already present in hash map
+            if(h.containsKey(preSum) == false)
+                h.put(preSum, i);
+
+
+            if(h.containsKey(preSum - sum))
+                res = Math.max(res, i - h.get(preSum - sum));
+
+        }
+
+        return res;
+
+    }
 
 
 
     public static void main(String args[]){
 
-        int arr[] ={5,2,3,4};
-		int result = longestSubArrayWithGivenSumNaive(arr, arr.length, 5);
+        int arr[] ={8,3,1,5,-6,6, 2,2};
+		int result = longestSubArrayWithGivenSum(arr, arr.length, 4);
         System.out.println(result);
 		
     }
