a28e302b4fee43ececcc59820c19fa37f5c70368
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-29 19:14:14 +0530

    Count Distinct in Every Window Efficient using Hashing and Window Sliding

--- hashing/CountDistinctInEveryWindow.java
+++ hashing/CountDistinctInEveryWindow.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class CountDistinctInEveryWindow {
 
     public static void countDistinctEveryWindowNaive(int arr[], int n, int k){
@@ -31,12 +33,43 @@ public class CountDistinctInEveryWindow {
         }
     }
 
+    public static void countDistinctEveryWindow(int arr[], int n, int k){
+        
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        // Compute the freqMap of first window elements
+        for(int i=0; i<k; i++){
+            freqMap.put(arr[i], freqMap.getOrDefault(arr[i], 0)+1);
+        }
+
+        // Print the size of freqMap
+        System.out.println(freqMap.size());
+
+        // Traverse through the remaining windows
+        for(int i=k; i<n; i++){
+
+            // Decrease the frequency of arr[i-k]
+            freqMap.put(arr[i-k], freqMap.get(arr[i-k])- 1);
+
+            // if the frequency equals 0, remove it from freqMap
+            if(freqMap.get(arr[i-k]) == 0)
+                freqMap.remove(arr[i-k]);
+
+            // If arr[i] does not exist in the freqMap, insert it. Else, increase it frequency
+            freqMap.put(arr[i], freqMap.getOrDefault(arr[i], 0)+1);
+
+            System.out.println(freqMap.size());
+        }
+
+
+
+    }
 
     public static void main(String args[]){
 
         // Two Binary Arrays
-        int arr[] = {10,10,5,3,20,5};
-        countDistinctEveryWindowNaive(arr, arr.length, 4);
+        int arr[] = {10,20,10,10,30,40};
+        countDistinctEveryWindow(arr, arr.length, 4);
     }
     
 }
