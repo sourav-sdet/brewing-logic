18dd0c8efa6cfca4c7a49ea275b7026944690c74
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-29 08:55:46 +0530

    Longest Common Span Efficient Solution

--- hashing/LongestCommonSpan.java
+++ hashing/LongestCommonSpan.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class LongestCommonSpan {
 
     public static int longestCommonSpanNaive(int arr1[], int arr2[]){
@@ -26,7 +28,40 @@ public class LongestCommonSpan {
     }
 
 
+    public static int longestCommonSpan(int arr1[], int arr2[]){
 
+        int n= arr1.length;
+
+        // STEP 1: Create an aux array temp[] to store arr1[] - arr2[]
+        int temp[]=new int[n];
+        for(int i=0; i<n; i++){
+            temp[i] = arr1[i] - arr2[i];
+        }
+
+        // STEP 2: Use length of longest sub array with given sum =0 in temp
+        int res = 0;
+        int preSum =0;
+        int sum=0;
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            preSum = preSum + temp[i];
+
+            if(preSum ==0)
+                res=i+1;
+
+            if(h.containsKey(preSum) == false)
+                h.put(preSum, i);
+
+            if(h.containsKey(preSum - sum)){
+                res = Math.max(res, i-h.get(preSum-sum));
+            }
+        }
+
+        return res;
+
+
+    }
 
 
 
@@ -35,7 +70,7 @@ public class LongestCommonSpan {
         // Two Binary Arrays
         int arr1[] = {0,1,0,0,0,0};
         int arr2[] = {1,0,1,0,0,1};
-        int result = longestCommonSpanNaive(arr1, arr2);
+        int result = longestCommonSpan(arr1, arr2);
         System.out.println(result);
     }
 
