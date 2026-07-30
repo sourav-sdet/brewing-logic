5322fd0b28f6c9c6531cb81f67cc0697fda4f86e
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-30 09:28:02 +0530

    More than n/k occurrences Efficient Solution

--- hashing/MoreThanNSlashKOccurrences.java
+++ hashing/MoreThanNSlashKOccurrences.java
@@ -1,6 +1,8 @@
 package hashing;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoreThanNSlashKOccurrences {
     
@@ -29,12 +31,26 @@ public class MoreThanNSlashKOccurrences {
 
     }
 
+    public static void moreThanNSlashKOccurrences(int arr[], int n, int k){
 
+        // Use a HashMap to store array element as KEY and its frequency as VALUE
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        // Traverse through the array and insert array elements and its frequencies into HashMap
+        for(int i=0; i<n; i++){
+            freq.put( arr[i], freq.getOrDefault(arr[i], 0)+1);
+        }
+
+        // Iterate through the HashMap and check if frequency of any element is > n/k
+        for( Map.Entry<Integer, Integer> entry: freq.entrySet() )
+            if(entry.getValue() > (n/k))
+                System.out.println(entry.getKey());
+
+    }
 
 
     public static void main(String args[]){
-
         int arr[] = {10,10,20,30,20,10,10};
-        moreThanNSlashKOccurrencesNaive(arr, arr.length, 2);
+        moreThanNSlashKOccurrences(arr, arr.length, 2);
     }
 }
