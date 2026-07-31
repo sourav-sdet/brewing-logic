6b6f519b4c7b081717f4bfe6c980c43ab2b4c187
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-07-31 20:25:53 +0530

    More Than n/k Moore's Solution Still Not Working

--- hashing/MoreThanNSlashKOccurrences.java
+++ hashing/MoreThanNSlashKOccurrences.java
@@ -2,6 +2,7 @@ package hashing;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class MoreThanNSlashKOccurrences {
@@ -49,12 +50,13 @@ public class MoreThanNSlashKOccurrences {
     }
 
     public static void moreThanNSlashKOccurrencesMoores(int arr[], int n, int k){
+        // This is the expected approach for small k value
         // Extension to Moore's Voting Algorithm
         // TWO PHASES
         // PHASE 1: FIND THE CANDIDATE ELEMENTS
 
         // Create an empty map
-        HashMap<Integer, Integer> h = new HashMap<>();
+        HashMap<Integer,Integer> h = new HashMap<>();
 
         // Traverse through the array arr from L --> R
         for(int i=0; i<n; i++){
@@ -64,7 +66,7 @@ public class MoreThanNSlashKOccurrences {
                 h.put(arr[i], h.get(arr[i]) + 1);
             }
 
-            // if arr[i] is not present in map and map has space (size < k)
+            // if arr[i] is not present in map and map has space (size < k), insert it to map with freq=1
             else if(h.size() < k-1){
                 h.put(arr[i], 1);
             }
@@ -72,11 +74,28 @@ public class MoreThanNSlashKOccurrences {
             // if arr[i] is not present in map and map has no space
             else {
                 // Decrement all element frequencies by 1
+                Iterator<Map.Entry<Integer, Integer>> iter = h.entrySet().iterator();
+                while(iter.hasNext()){
+                    Map.Entry<Integer, Integer> entry = iter.next();
+                    int freq = entry.getValue() -1;
+
+                    if(freq == 0)
+                        iter.remove();
+                    else 
+                        entry.setValue(freq);
+                }
                 
             }
 
         }
 
+
+
+        // PHASE 2: Print elements whose frequency is greater than n/k
+        for(Map.Entry<Integer, Integer> e: h.entrySet()){
+            if(e.getValue() > (n/k))
+                System.out.println(e.getKey());
+        }
     }
 
     public static void main(String args[]){
