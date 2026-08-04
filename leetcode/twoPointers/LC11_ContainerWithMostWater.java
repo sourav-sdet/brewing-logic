c2ff79ee2c34541608b61a20aed19f2e9c410834
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-04 21:07:34 +0530

    LC11 Container With Most Water Skeleton

---
+++ leetcode/twoPointers/LC11_ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package leetcode.twoPointers;
+
+import java.util.List;
+
+public class LC11_ContainerWithMostWater {
+
+    public static int maxArea(int[] heights){
+
+        
+
+    }
+
+
+
+    public static void main(String args[]){
+        int[] heights = {1,8,6,2,5,4,8,3,7};
+        int result=maxArea(heights);
+        System.out.println(result);
+
+    }
+    
+}
