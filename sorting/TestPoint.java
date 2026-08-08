343286ef88ea568759a28d25a990b31eb150464b
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-08 08:54:47 +0530

    Correct Order of printing

--- sorting/TestPoint.java
+++ sorting/TestPoint.java
@@ -12,7 +12,9 @@ public class TestPoint {
 
         Arrays.sort(arr);
 
-        System.out.println(Arrays.toString(arr));
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i].x + " "+arr[i].y);
+        }
 
 
     }
