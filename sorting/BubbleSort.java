00a364ebc36a03a5f7629e418992ffd011044931
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-13 17:32:10 +0530

    Optimized Bubble Sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -13,8 +13,12 @@ public class BubbleSort {
         for(int i=0; i<n-1; i++){
 
             // j - to compare adjacent elements
-            // Optimization - If we are at ith iteration, i elements are already fixed to their
+            // Optimization 1 - If we are at ith iteration, i elements are already fixed to their
             // respective positions - i largest elements
+
+            // Optimation 2 - If array is already sorted/becomes sorted mid way
+            boolean flag = false;
+
             for(int j=0; j<n-i-1; j++){
                 if(arr[j] > arr[j+1]){
 
@@ -22,8 +26,13 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+
+                    flag = true;
                 }
             }
+
+            if(flag == false)
+                break;
 
         }
 
