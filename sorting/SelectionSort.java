74fcf16c8bf6469de7b91acdcd89304cbc842c4e
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-15 18:04:42 +0530

    Selection Sort Efficient

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -31,9 +31,42 @@ public class SelectionSort {
 
     }
 
+    public static void selectionSort(int arr[], int n){
+        
+        // Traverse through the input array
+        // Find out the minimum element
+        // Instead of storing it in a temp array, swap it with the current element
+
+        // At any point, we maintain two parts- Sorted and Unsorted
+        // We need to find out min element in the Unsorted Part
+
+        // Elements from 0 to i are sorted
+        // Elements from i+1 to n-1 are yet to be sorted
+        for(int i=0; i<n-1; i++){
+
+            // Find min element from Unsorted part
+            int minIndex=i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
+        }
+
+
+
+    }
+
+
+
     public static void main(String args[]){
 
-        int[] arr = {10,8,20,5};
+        int[] arr = {10,5,8,20,2,18};
         selectionSortNaive(arr,arr.length);
 
         for(int i=0; i<arr.length; i++){
