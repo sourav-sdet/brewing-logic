c11165326d718a78133ddb91b60838149d4eb83c
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-21 19:18:28 +0530

    Insertion Sort

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,45 @@
+package sorting;
+
+public class InsertionSort {
+
+    public static void insertionSort(int arr[], int n){
+
+        // For every array, maintain a sorted half and a non sorted half
+        // Start iterating from the second element since first element is already sorted
+        for(int i=1; i<n; i++){
+
+            int key = arr[i];
+
+            // For every i, go to its left side and find its correct position in the sorted half
+            int j=i-1;
+
+            // Move to the left side till elements are greater than key
+            while(j >= 0 && arr[j] > key){
+
+                // Move elements one position ahead
+                arr[j+1] = arr[j];
+                j--;
+
+            }
+            arr[j+1] = key;
+        }
+
+
+
+    }
+    
+    public static void main(String args[]){
+
+        int[] arr = {10,8,20,5};
+        insertionSort(arr,arr.length);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+
+
+
+    
+}
