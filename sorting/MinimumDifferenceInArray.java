47e37b964ceee3a8bb52c8bc821960d53b32b64b
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-27 21:13:20 +0530

    Minimum Difference in Array Efficient Solution

--- sorting/MinimumDifferenceInArray.java
+++ sorting/MinimumDifferenceInArray.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class MinimumDifferenceInArray {
 
     public static int getMinDifferenceNaive(int arr[], int n){
@@ -18,11 +20,27 @@ public class MinimumDifferenceInArray {
 
     }
 
+
+    public static int getMinDifference(int arr[], int n){
+
+        int minDiff = Integer.MAX_VALUE;
+
+        // Step 1: Sort the Input Array
+        Arrays.sort(arr);
+
+        for(int i=1; i<n; i++){
+            minDiff = Math.min( Math.abs(arr[i] - arr[i-1]), minDiff);
+        }
+
+        return minDiff;
+        
+    }
+
     public static void main(String args[]){
 
-        //int arr[] = {1,8,12,5,18};
-        int arr[] = {5,3,8};
-        System.out.println(getMinDifferenceNaive(arr, arr.length));
+        int arr[] = {1,8,12,5,18};
+        //int arr[] = {5,3,8};
+        System.out.println(getMinDifference(arr, arr.length));
 
     }
     
