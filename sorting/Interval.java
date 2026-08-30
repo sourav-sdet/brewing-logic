de8d78e34b456313b097b01ecb5fbc01d5963026
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-08-30 20:09:08 +0530

    Implementation of compareTo for Interval Class

--- sorting/Interval.java
+++ sorting/Interval.java
@@ -1,6 +1,6 @@
 package sorting;
 
-public class Interval {
+public class Interval implements Comparable<Interval> {
 
     int start;
     int end;
@@ -8,6 +8,12 @@ public class Interval {
     public Interval(int start, int end){
         this.start = start;
         this.end = end;
+    }
+
+    public int compareTo(Interval i1){
+
+        return this.start - i1.start;
+
     }
     
 }
