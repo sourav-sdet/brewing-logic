c9191c9b7868d89fb4c99e8e808fbbf88832c7fe
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-09 21:16:02 +0530

    GCD using Basic Euclidean Algorithm

--- mathematics/GCD.java
+++ mathematics/GCD.java
@@ -18,12 +18,24 @@ public class GCD {
 
     }
 
+    public static int gcd(int a, int b){
 
+        while(a != b){
+
+            if(a > b)
+                a = a-b;
+            else 
+                b= b-a;
+        }
+
+        return a;
+
+    }
 
     public static void main(String args[]){
 
         int a=4, b=6;
-        System.out.println(gcdNaive(a,b));
+        System.out.println(gcd(a,b));
 
 
     }
