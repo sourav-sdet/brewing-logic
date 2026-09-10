f8d1436432f244080798b148f7b8e4d6de2b2fb0
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-10 18:17:10 +0530

    All Divisors in Unsorted Order

--- mathematics/AllDivisors.java
+++ mathematics/AllDivisors.java
@@ -12,13 +12,28 @@ public class AllDivisors {
     }
 
 
+    public static void allDivisorsNotInSortedOrder(int num){
+
+
+        for(int i=1; i*i <= num; i++){
+            if(num % i == 0){
+                System.err.println(i);
+                if(i != num/i)
+                    System.err.println(num/i);
+            }
+        }
+
+
+    }
+
+
 
 
 
 
     public static void main (String args[]){
-        int num = 15;
-        allDivisorsNaive(num);
+        int num = 25;
+        allDivisorsNotInSortedOrder(num);
 
     }
     
