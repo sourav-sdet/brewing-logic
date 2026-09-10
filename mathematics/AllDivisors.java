08b3e241db004697fed416f2912364753dc12eed
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-10 18:21:28 +0530

    All Divisors in Sorted Order

--- mathematics/AllDivisors.java
+++ mathematics/AllDivisors.java
@@ -26,14 +26,29 @@ public class AllDivisors {
 
     }
 
+    public static void allDivisors(int num){
+
+        int i;
+
+        // Print all divisors from 1(inclusive) to root num(exclusive)
+        for(i=1; i*i < num; i++){
+            if(num % i == 0)
+                System.out.println(i);
+        }
+
+        // Print all divisors from root n(inclusive) to num(inclusive)
+        for(;i>=1; i--){
+            if(num % i == 0)
+                System.out.println(num/i);
+        }
 
 
-
+    }
 
 
     public static void main (String args[]){
         int num = 25;
-        allDivisorsNotInSortedOrder(num);
+        allDivisors(num);
 
     }
     
