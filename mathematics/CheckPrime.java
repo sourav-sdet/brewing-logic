f89493fb49e6b6f2e19f5851738be2b80660b7a2
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-10 17:50:40 +0530

    Check for Prime Efficient Solution

--- mathematics/CheckPrime.java
+++ mathematics/CheckPrime.java
@@ -17,13 +17,26 @@ public class CheckPrime {
     }
 
 
+    public static boolean isPrime(int num){
 
+        if(num == 1)
+            return false;
+
+        // Traverse from 2 to root num
+        for(int i=2; i*i <= num; i++){
+            if(num % i == 0)
+                return false;
+        }
+
+        return true;
+
+    }
 
 
 
     public static void main (String args[]){
         int num = 13;
-        System.out.println(isPrimeNaive(num));
+        System.out.println(isPrime(num));
 
     }
     
