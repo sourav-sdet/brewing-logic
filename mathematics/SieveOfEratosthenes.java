ec42050e0fe93b5ee9ebb8ff75df897d0f2349cf
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-12 09:19:36 +0530

    Sieve Algorithm Implementation

--- mathematics/SieveOfEratosthenes.java
+++ mathematics/SieveOfEratosthenes.java
@@ -1,5 +1,7 @@
 package mathematics;
 
+import java.util.Arrays;
+
 public class SieveOfEratosthenes {
 
     public static void primeNumbersSmallerOrEqualNaive(int num){
@@ -14,9 +16,40 @@ public class SieveOfEratosthenes {
 
     }
 
+
+    public static void primeNumbersSmallerOrEqual(int num){
+
+        // SIEVE OF ERATOSTENES 
+        // Create a boolean array of size n+1
+        // Initially all elements in boolean array is marked as true
+        // At the end of algorithm, composites will be marked as false
+        // Primes will remain as true
+
+        boolean isPrime[] = new boolean[num + 1];
+        Arrays.fill(isPrime, true);
+
+
+        for(int i=2; i*i <=num; i++){
+            if(CheckPrime.isPrime(i)){
+                for(int j= 2*i; j<=num; j=j+i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for(int i=2; i<=num; i++){
+            if(isPrime[i]){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+
+
     public static void main(String args[]){
         int num = 23;
-        primeNumbersSmallerOrEqualNaive(num);
+        primeNumbersSmallerOrEqual(num);
     }
     
 }
