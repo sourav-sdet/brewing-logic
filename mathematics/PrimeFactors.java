cb5b15188fbbe9969b06d058a390f69c489112cd
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-12 08:58:53 +0530

    Prime Factor Efficient Solution

--- mathematics/PrimeFactors.java
+++ mathematics/PrimeFactors.java
@@ -21,12 +21,31 @@ public class PrimeFactors {
 
     }
 
+    // Efficient Solution Idea: Divisors of a number always exists in pairs
+    public static void primeFactors(int num){
+
+        if (num <=1)
+            return;
+
+        for(int i=2; i*i <= num; i++){
+            while(num % i == 0){
+                System.err.println(i);
+                num = num / i;
+            }
+        }
+
+        // When largest prime factor has power 1, falls into this case
+        if(num > 1)
+            System.out.println(num);
+
+    }
+
 
 
 
     public static void main(String args[]){
-        int num = 315;
-        primeFactorsNaive(num);
+        int num = 450;
+        primeFactors(num);
     }
     
 }
