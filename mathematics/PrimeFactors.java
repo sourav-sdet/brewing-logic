dcd2440cf0664cee4d4b1a4640fae1ffa09ce29a
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2026-09-12 09:04:19 +0530

    Prime Factors of a Number Further Optimization

--- mathematics/PrimeFactors.java
+++ mathematics/PrimeFactors.java
@@ -40,12 +40,46 @@ public class PrimeFactors {
 
     }
 
+    // More efficient implementation to save iterations
+    public static void primeFactorsOptimized(int num){
+
+        if (num <=1)
+            return;
+
+        while(num % 2 == 0){
+            System.out.println(2);
+            num = num/2;
+        }
+
+        while(num % 3 == 0){
+            System.out.println(3);
+            num = num/3;
+        }
+
+        for(int i=5; i*i <= num; i=i+6){
+            while(num % i == 0){
+                System.out.println(i);
+                num = num / i;
+            }
+
+            while(num % (i+2) == 0){
+                System.out.println(i+2);
+                num = num / (i+2);
+            }
+        }
+
+        // When largest prime factor has power 1, falls into this case
+        if(num > 1)
+            System.out.println(num);
+
+    }
+
 
 
 
     public static void main(String args[]){
         int num = 450;
-        primeFactors(num);
+        primeFactorsOptimized(num);
     }
     
 }
